b8d41e1dfbc73214f098ec15ed101cf437322734
Metavoyager <metavoyager@example.com>
2026-03-14 17:48:39 +0100

    refactor(app): simplify wallet adapter memo deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
       new PhantomWalletAdapter(),
       new SolflareWalletAdapter(),
     ],
-    [network]
+    []
   );
 
   return (
